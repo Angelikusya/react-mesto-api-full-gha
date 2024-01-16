d566c4eb139eb24bd2929ff4fbea668b6e27d449
Anzhelika <angelikachashchina@gmail.com>
2024-01-17 01:12:14 +0300

    реьвю

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = process.env.JWT_SECRET;
+const { JWT_SECRET = 'SECRET_KEY' } = process.env;
 
-const generateToken = (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: '7d' });
+const generateToken = (payload) => jwt.sign(payload, JWT_SECRET, { expiresIn: '7d' });
 
 module.exports = generateToken;
